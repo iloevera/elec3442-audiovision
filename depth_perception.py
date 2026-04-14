6403688a0c2a256b7140e1453f38fad891c98b7b
mahmoodafif <158798007+mahmoodafif@users.noreply.github.com>
2026-04-14 21:37:30 +0800

    Update depth_perception.py

--- depth_perception.py
+++ depth_perception.py
@@ -1,1 +1,16 @@
+import cv2
+from picamera2 import Picamera2
+import time
 
+camera0 = Picamera2(cam_num=0)
+camera1 = Picamera2(cam_num=1)
+
+config0 = camera0.create_preview_configuration(main={"size": (640, 480)})
+config1 = camera1.create_preview_configuration(main={"size": (640, 480)})
+
+camera0.configure(config0)
+camera1.configure(config1)
+
+camera0.start()
+camera1.start()
+time.sleep(1)
